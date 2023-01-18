a1ec65b1ed2f1bb1b79dbaa08cc795ae7b79af66
Tework123 <daniliesaec@gmail.com>
2023-01-18 16:23:14 +0300

    work for chat and user.

--- venv/main.py
+++ venv/main.py
@@ -24,47 +24,104 @@ def send_chat(id, text):
     vk.messages.send(chat_id=id, message=text, random_id=get_random_id())
 
 
+def send_to_user(id, text, keyboard=None):
+    if keyboard == None:
+        vk.messages.send(user_id=id, message=text, random_id=get_random_id())
+    if keyboard == 'keyboard_start':
+        vk.messages.send(user_id=id, message=text, random_id=get_random_id(), keyboard=my_keyboard.get_keyboard_start())
+    if keyboard == 'keyboard_other':
+        vk.messages.send(user_id=id, message=text, random_id=get_random_id(), keyboard=my_keyboard.get_keyboard_other())
+    if keyboard == 'keyboard_attach':
+        vk.messages.send(user_id=id, message=text, random_id=get_random_id(),
+                         keyboard=my_keyboard.get_keyboard_attach())
+    if keyboard == 'keyboard_put':
+        vk.messages.send(user_id=id, message=text, random_id=get_random_id(), keyboard=my_keyboard.get_keyboard_put())
+
+
 # отправка сообщений с клавиатурой
-def send_new_keyboard(id, text, keyboard):
+def send_chat(id, text, keyboard=None):
+    if keyboard == None:
+        vk.messages.send(chat_id=id, message=text, random_id=get_random_id())
     if keyboard == 'keyboard_start':
         vk.messages.send(chat_id=id, message=text, random_id=get_random_id(), keyboard=my_keyboard.get_keyboard_start())
-    if keyboard == 'keyboard_one':
-        vk.messages.send(chat_id=id, message=text, random_id=get_random_id(), keyboard=my_keyboard.get_keyboard_one())
-    if keyboard == 'keyboard_two':
-        vk.messages.send(chat_id=id, message=text, random_id=get_random_id(), keyboard=my_keyboard.get_keyboard_two())
+    if keyboard == 'keyboard_other':
+        vk.messages.send(chat_id=id, message=text, random_id=get_random_id(), keyboard=my_keyboard.get_keyboard_other())
+    if keyboard == 'keyboard_attach':
+        vk.messages.send(chat_id=id, message=text, random_id=get_random_id(),
+                         keyboard=my_keyboard.get_keyboard_attach())
+    if keyboard == 'keyboard_put':
+        vk.messages.send(chat_id=id, message=text, random_id=get_random_id(), keyboard=my_keyboard.get_keyboard_put())
 
 
-def send_photo(id, url):
-    vk.messages.send(chat_id=id, attachment=url, random_id=get_random_id())
+def send_photo(id, text, url):
+    vk.messages.send(chat_id=id, message=text, attachment=url, random_id=get_random_id())
 
 
 # прослушка и выделение id отправителя, id чата, нахождение требуемого ответа из допустимых
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
+        # если получает сообщение из беседы
         if event.from_chat:
+            print('chat')
             id = event.chat_id
             id_user = event.message.get('from_id')
             msg = event.message.get('text')
-            peer_id = event.message.get('peer_id')  # также c attachments
-            bot_adress = '[club218249845|@public218249845]'
+            # peer_id = event.message.get('peer_id')  # также c attachments
+            # print(peer_id)
 
-            # словари с ответными действиями
+            bot_adress = '[club218249845|@public218249845]'
             collection = {
                 f"{bot_adress} Отправить мем": (id, f' Отправляю мем'),
                 f"{bot_adress} Получить расписание": (id, f' Выгружаю расписание'),
+                # клавиатура "добавить метку"
+                f"{bot_adress} Добавить метку": (id, f'Добавить метку')
             }
-            collection_keyboard = {
-                f"{bot_adress} Назад": (id, f' Назад', 'keyboard_start'),
-                f"{bot_adress} Разное": (id, f' Разное', 'keyboard_one'),
-                f"{bot_adress} Материалы": (id, f' Выберите дальнейшее действие', 'keyboard_two')
+            collection_keyboard_start = {
+                f"{bot_adress} Назад": (id, f'Назад', 'keyboard_start'),
+                f"{bot_adress} Разное": (id, f'Разное', 'keyboard_other'),
+                f"{bot_adress} Материалы": (id, f'Выберите дальнейшее действие', 'keyboard_attach'),
+                # клавиатура "материалы"
+                f"{bot_adress} Добавить метку": (id, f'Добавить метку', 'keyboard_put')
             }
 
             if msg == '.бот':
-                send_new_keyboard(id, 'Hello my boy', 'keyboard_start')
+                send_chat(id, 'Hello my boy', 'keyboard_start')
             if msg in collection:
                 send_chat(collection[msg][0], collection[msg][1])
-            if msg in collection_keyboard:
-                send_new_keyboard(collection_keyboard[msg][0], collection_keyboard[msg][1], collection_keyboard[msg][2])
+            if msg in collection_keyboard_start:
+                send_chat(collection_keyboard_start[msg][0],
+                          collection_keyboard_start[msg][1],
+                          collection_keyboard_start[msg][2])
+
+        # если получает сообщение из личного сообщения
+        if event.from_user:
+            print('user')
+            id_user = event.message.get('from_id')
+            id = id_user
+
+            collection = {
+                f"Отправить мем": (id, f' Отправляю мем'),
+                f"Получить расписание": (id, f' Выгружаю расписание'),
+                # клавиатура "добавить метку"
+                f"Добавить метку": (id, f'Добавить метку')
+            }
+            collection_keyboard_start = {
+                f"Назад": (id, f'Назад', 'keyboard_start'),
+                f"Разное": (id, f'Разное', 'keyboard_other'),
+                f"Материалы": (id, f'Выберите дальнейшее действие', 'keyboard_attach'),
+                # клавиатура "материалы"
+                f"Добавить метку": (id, f'Добавить метку', 'keyboard_put')
+            }
+
+            msg = event.message.get('text')
+            if msg == '.бот':
+                send_to_user(id, 'Hello my boy', 'keyboard_start')
+            if msg in collection:
+                send_to_user(collection[msg][0], collection[msg][1])
+            if msg in collection_keyboard_start:
+                send_to_user(collection_keyboard_start[msg][0],
+                             collection_keyboard_start[msg][1],
+                             collection_keyboard_start[msg][2])
 
 if __name__ == '__main__':
     main()
--- venv/my_keyboard.py
+++ venv/my_keyboard.py
@@ -1,4 +1,7 @@
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+import wiki_parcer
+import pathlib
+from pathlib import Path
 
 
 # модуль генерации клавиатур
@@ -18,23 +21,39 @@ def get_keyboard_start():
 
 
 # клавиатура "разное"
-def get_keyboard_one():
+def get_keyboard_other():
     key_board = VkKeyboard(inline=False)
 
     key_board.add_button('Назад', color=VkKeyboardColor.PRIMARY)
     key_board.add_button('Погода', color=VkKeyboardColor.PRIMARY)
+    key_board.add_button('Погода', color=VkKeyboardColor.PRIMARY)
+    key_board.add_line()
+    key_board.add_openlink_button('Случайная статья с википедии', wiki_parcer.wiki_parcer())
+
     return key_board.get_keyboard()
 
 
 # клавиатура "материалы"
-def get_keyboard_two():
+def get_keyboard_attach():
     key_board = VkKeyboard(inline=False)
 
-    key_board.add_button('Добавить', color=VkKeyboardColor.PRIMARY)  # по сообщению
-    key_board.add_button('Получить', color=VkKeyboardColor.PRIMARY)  # хз
+    key_board.add_button('Добавить метку', color=VkKeyboardColor.PRIMARY)
+    key_board.add_button('Получить', color=VkKeyboardColor.PRIMARY)
     key_board.add_button('Назад', color=VkKeyboardColor.PRIMARY)
     key_board.add_line()
     key_board.add_openlink_button('Твоя ссылочка на диск',
-                                  'https://disk.yandex.ru/client/disk/%D0%9F-48%2C%20%D0%9F-42')  # по сообщению дает клаву
+                                  'https://disk.yandex.ru/client/disk/%D0%9F-48%2C%20%D0%9F-42')
+
+    return key_board.get_keyboard()
+
+
+def get_keyboard_put():
+    key_board = VkKeyboard(inline=False)
+
+    key_board.add_button('Козловский', color=VkKeyboardColor.PRIMARY)
+    key_board.add_button('Староверов', color=VkKeyboardColor.PRIMARY)
+    key_board.add_line()
+    key_board.add_button('Диплом', color=VkKeyboardColor.PRIMARY)
+    key_board.add_button('Назад', color=VkKeyboardColor.PRIMARY)
 
     return key_board.get_keyboard()
